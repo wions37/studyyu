40cc7e9916a829fe67a061e99643b3d9a5e692c2
wions37 <gpdnjs0307@gmail.com>
2024-06-05 21:52:10 +0900

    check

---
+++ src/perator/oper1.java
@@ -0,0 +1,47 @@
+package perator;
+
+public class oper1 {
+    //    public static void main(String[] args) {
+//        int a =8;
+//        int b =3;
+//        int sum = a+b;
+//
+//        System.out.println("a+b="+sum);
+//
+//
+//        int diff = a - b;
+//        System.out.println("a -b="+diff);
+//
+//        int multi = a*b;
+//        System.out.println("a*b="+multi);
+//
+//        int div = a/b;
+//        System.out.println("a/b="+div);
+//
+//        int mod =a%b;
+//        System.out.println("a%b="+mod);
+//
+//    }
+//    public static void main(String[] args) {
+//        String result = "hello" + "worid";
+//        System.out.println(result);
+//
+//        String s1 ="string1";
+//        String s2 ="string2";
+//        String result2 = s1+ s2 ;
+//
+//        System.out.println(s1+s2);
+//        System.out.println(result2);
+//
+//        String result3 = "a+b" +10;
+//        System.out.println(result3);
+//
+//        int num =20;
+//        String str ="a+b=";
+//        String result4 = str+num;
+//        System.out.println(result4);
+//
+//    }
+
+
+}
