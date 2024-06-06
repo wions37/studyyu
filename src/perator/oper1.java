e34a09abbcdbff4e233ba22ee30ed057778ba74b
wions37 <gpdnjs0307@gmail.com>
2024-06-06 12:36:55 +0900

    check

--- src/perator/oper1.java
+++ src/perator/oper1.java
@@ -1,47 +1,216 @@
 package perator;
 
 public class oper1 {
-    //    public static void main(String[] args) {
-//        int a =8;
-//        int b =3;
-//        int sum = a+b;
+    //    //    public static void main(String[] args) {
+////        int a =8;
+////        int b =3;
+////        int sum = a+b;
+////
+////        System.out.println("a+b="+sum);
+////
+////
+////        int diff = a - b;
+////        System.out.println("a -b="+diff);
+////
+////        int multi = a*b;
+////        System.out.println("a*b="+multi);
+////
+////        int div = a/b;
+////        System.out.println("a/b="+div);
+////
+////        int mod =a%b;
+////        System.out.println("a%b="+mod);
+////
+////    }
+////    public static void main(String[] args) {
+////        String result = "hello" + "worid";
+////        System.out.println(result);
+////
+////        String s1 ="string1";
+////        String s2 ="string2";
+////        String result2 = s1+ s2 ;
+////
+////        System.out.println(s1+s2);
+////        System.out.println(result2);
+////
+////        String result3 = "a+b" +10;
+////        System.out.println(result3);
+////
+////        int num =20;
+////        String str ="a+b=";
+////        String result4 = str+num;
+////        System.out.println(result4);
+////
+////    }
+////    public static void main(String[] args) {
+////        int sum1 = 1+2*3;
+////        int sum2 = (1+2)*3;
+////        System.out.println("sum1="+sum1);
+////        System.out.println("sum2="+sum2);
+////    }
+////    public static void main(String[] args) {
+////        int sum3 =2*2+3*3 ;
+////        int sum4 =(2*2)+(3*3) ;
+////        System.out.println("sum3 = " + sum3);
+////        System.out.println("sum4 = " + sum4);
+////
+////    }
+////
+////    public static void main(String[] args) {
+////        int a =0;
+////        a= a+1;
+////        System.out.println("a="+a);
+////
+////        a= a+1;
+////        System.out.println("a="+a);
+////
+////        ++a;
+////        System.out.println("a="+a);
+////
+////        --a;
+////        System.out.println("a="+a);
+////
+////
+////    }
+////    public static void main(String[] args) {
+//////        int a=1;
+//////        int b=0;
+//////
+//////        b= ++a;
+////        // a=a+1  > a++
+////        // b= a  이 두줄이 합쳐진거
+//////        System.out.println("a="+a+",b="+b);
+////
+////        int a=1;
+////        int b=0;
+////
+////        b= a++;
+////
+////        // b= a  이 두줄이 합쳐진거
+////        // a=a+1  > a++
+////        System.out.println("a="+a+",b="+b);
+////    }
+////
+////    public static void main(String[] args) {
+////        int a = 2;
+////        int b = 3;
+////
+////        System.out.println(a == b);
+////        System.out.println(a != b);
+////        System.out.println(a > b);
+////        System.out.println(a < b);
+////        System.out.println(a >= b);
+////        System.out.println(a <= b);
+////
+////        boolean result = a == b;
+////        System.out.println(result);
+////
+////    }
+//    public static void main(String[] args) {
+//        String str1 = "문자열1";
+//        String str2 = "문자열2";
 //
-//        System.out.println("a+b="+sum);
+//        boolean result1 ="hello".equals("hello");
+//        boolean result2 = str1.equals("문자열1");
+//        boolean result3 = str1.equals(str2);
 //
-//
-//        int diff = a - b;
-//        System.out.println("a -b="+diff);
-//
-//        int multi = a*b;
-//        System.out.println("a*b="+multi);
-//
-//        int div = a/b;
-//        System.out.println("a/b="+div);
-//
-//        int mod =a%b;
-//        System.out.println("a%b="+mod);
+//        System.out.println(result1);
+//        System.out.println(result2);
+//        System.out.println(result3);
 //
 //    }
 //    public static void main(String[] args) {
-//        String result = "hello" + "worid";
+//        System.out.println("&&:AND 연산");
+//        System.out.println(true && true);
+//        System.out.println(true && false);
+//        System.out.println(false && false);
+//
+//        System.out.println("|| or 연산");
+//        System.out.println(true || true);
+//        System.out.println(true || false);
+//        System.out.println(false || false);
+//
+//        System.out.println("! 부정 연산");
+//        System.out.println(!true);
+//        System.out.println(!false);
+//
+//        System.out.println("변수 활용");
+//        boolean a =true;
+//        boolean b =false;
+//        System.out.println(a && b);
+//        System.out.println(a || b);
+//        System.out.println(!a);
+//        System.out.println(!b);
+//
+//
+//    }
+//    public static void main(String[] args) {
+//        int a =15;
+//        boolean result = a > 10 && a < 20;
+//        boolean result1 = 10 < a && a < 30;
+//        System.out.println("result= " + result);
+//    }
+//    public static void main(String[] args) {
+//        int a= 5;
+//        a += 3 ;
+//        a -= 2;
+//        a *= 4;
+//        a /=3;
+//        a %= 5;
+//
+//        System.out.println(a);
+//    }
+//    public static void main(String[] args) {
+//        int num1 =10;
+//        int num2 =20;
+//        int num3 =30;
+//
+//        int sum = num3 + num2 + num3;
+//
+//        int average = sum / 3;
+//
+//        System.out.println("sum = " + sum);
+//        System.out.println("average = " + average);
+//    }
+
+//    public static void main(String[] args) {
+//        double val1 = 1.5;
+//        double val2 = 2.5;
+//        double val3 = 3.5;
+//
+//        double sum = val1 + val2 + val3;
+//        double average = sum / 3;
+//
+//        System.out.println(sum);
+//        System.out.println(average);
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        int score = 70;
+//            boolean result = (score >= 80 && score <= 100);
 //        System.out.println(result);
 //
-//        String s1 ="string1";
-//        String s2 ="string2";
-//        String result2 = s1+ s2 ;
-//
-//        System.out.println(s1+s2);
-//        System.out.println(result2);
-//
-//        String result3 = "a+b" +10;
-//        System.out.println(result3);
-//
-//        int num =20;
-//        String str ="a+b=";
-//        String result4 = str+num;
-//        System.out.println(result4);
 //
 //    }
 
+    //    public static void main(String[] args) {
+//        int age = 15;
+//
+//      if (age >= 18) {
+//          System.out.println("성인");
+//      }
+//      if (age < 18) {
+//          System.out.println("미성년");
+//      }
+//    }
+//    public static void main(String[] args) {
+//        int age = 20;
+//        if (age >= 18) {
+//            System.out.println("성인");
+//        } else {
+//            System.out.println("미성년");
+//        }
+//    }
 
 }
